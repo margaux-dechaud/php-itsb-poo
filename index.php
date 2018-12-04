d1208389704aa2cc92f4fe609281db56e7a87ae2
traderbook <contact@traderbook.net>
2018-12-04 11:57:48 +0100

    Ajout des getters et setters

--- index.php
+++ index.php
@@ -4,12 +4,30 @@ class Person {
     private $firstname;
     private $lastname;
 
+    /**
+     * Création d'un setter (ou mutateur)
+     *
+     * @param $pFirstname
+     */
     public function setFirstname($pFirstname) {
         $this->firstname = $pFirstname;
     }
 
+    /**
+     * Création d'un getter (ou accesseur)
+     *
+     * @return mixed
+     */
+    public function getFirstname() {
+        return $this->firstname;
+    }
+
     public function setLastname($pLastname) {
         $this->lastname = $pLastname;
+    }
+
+    public function getLastname() {
+        return $this->lastname;
     }
 }
 
@@ -20,9 +38,14 @@ $person1 = new Person();
 $person1->setFirstname("John");
 $person1->setLastname("Doe");
 
-var_dump($person1);
+var_dump($person1->getFirstname());
+var_dump($person1->getLastname());
 
 //$person2 = new Person();
 //$person2->firstname = "Jane";
 //$person2->lastname = "Die";
+
+//$person2->setFirstname("Jane");
+//$person2->setLastname("Die");
+//
 //var_dump($person2);
