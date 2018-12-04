22eca158be1df7f2855989432a417b3fd4c62a51
traderbook <contact@traderbook.net>
2018-12-04 11:44:13 +0100

    Création d'une class person avec ses instances

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php
 
-var_dump("coucou");
+class Person {
+    private $firstname;
+    private $lastname;
+
+    public function setFirstname($pFirstname) {
+        $this->firstname = $pFirstname;
+    }
+
+    public function setLastname($pLastname) {
+        $this->lastname = $pLastname;
+    }
+}
+
+$person1 = new Person();
+//$person1->firstname = "John";
+//$person1->lastname = "Doe";
+
+$person1->setFirstname("John");
+$person1->setLastname("Doe");
+
+var_dump($person1);
+
+//$person2 = new Person();
+//$person2->firstname = "Jane";
+//$person2->lastname = "Die";
+//var_dump($person2);
