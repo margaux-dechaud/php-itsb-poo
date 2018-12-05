698a2d394e0f4cd8cc482e3a511efc9cc18d758d
traderbook <contact@traderbook.net>
2018-12-05 11:06:35 +0100

    Création d'une extension de classe

--- index.php
+++ index.php
@@ -1,1 +1,34 @@
 <?php
+
+class Mammal {
+    private $years = 0;
+
+    /**
+     * @return int
+     */
+    public function getYears(): int
+    {
+        return $this->years;
+    }
+
+    /**
+     * @param int $years
+     */
+    public function setYears(int $years): void
+    {
+        $this->years = $years;
+    }
+}
+
+class Person extends Mammal {
+    private $firstname;
+    private $lastname;
+}
+
+class Dog extends Mammal {
+    private $name;
+}
+
+$person = new Person(1);
+
+var_dump($person);
