0caa690aa44dc5889eec686795e43c2c5eec6018
traderbook <contact@traderbook.net>
2018-12-05 14:49:34 +0100

    J'ai pas d'idée

--- index.php
+++ index.php
@@ -1,1 +1,29 @@
 <?php
+
+abstract class Vehicule {
+    public function avancer() {
+        var_dump("J'avance...");
+    }
+}
+
+class Car extends Vehicule { }
+class Bike extends Vehicule { }
+
+class User {
+    private $vehicule;
+
+    public function setVehicule(Vehicule $pVehicule) {
+        $this->vehicule = $pVehicule;
+    }
+
+    public function avancer() {
+        $this->vehicule->avancer();
+    }
+}
+
+$car = new Car();
+
+$user = new User();
+$user->setVehicule($car);
+
+$user->avancer();
