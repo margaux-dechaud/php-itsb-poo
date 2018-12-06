222e08bfb93b21a550c93d17c8f6611453eac261
traderbook <contact@traderbook.net>
2018-12-06 10:31:26 +0100

    Création d'une requête de selection avec création d'un objet de type Client

--- index.php
+++ index.php
@@ -9,6 +9,20 @@ $host = '172.17.0.3'; // 'localhost';
 $port = 3306;
 $database = 'test';
 
+$clients = array();
+
+class Client {
+    private $id;
+    private $firstname;
+    private $lastname;
+
+    public function __construct(int $id, string $firstname, string $lastname) {
+        $this->id = $id;
+        $this->firstname = $firstname;
+        $this->lastname = $lastname;
+    }
+}
+
 try {
     $pdo = new PDO("mysql:host=$host;port=$port;dbname=$database",
         'bibi',
@@ -16,11 +30,26 @@ try {
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $pdo->exec("INSERT INTO clients (firstname, lastname) VALUES ('john', 'doe');");
+//    $pdo->exec("INSERT INTO clients (firstname, lastname) VALUES ('john', 'doe');");
+//    var_dump("Le dernier ID est : " . $pdo->lastInsertId());
 
-    var_dump("Le dernier ID est : " . $pdo->lastInsertId());
+    $stmt = $pdo->query("SELECT * FROM clients;");
 
-    var_dump($pdo);
+//    var_dump($stmt->fetchObject());
+
+    while (($row = $stmt->fetch(PDO::FETCH_ASSOC)) !== false) {
+        $client = new Client(
+            $row['id'],
+            $row['firstname'],
+            $row['lastname']
+        );
+
+        $clients[] = $client;
+    }
+
+    var_dump($clients);
+
+//    var_dump($stmt);
 } catch (PDOException $e) {
     var_dump($e->getMessage());
 //    var_dump("Bad credentials");
